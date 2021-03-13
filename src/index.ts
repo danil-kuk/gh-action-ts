1d68d91e3fb1a997d7815c1dad432a8db1f5bca6
danil-kuk <danilkuk5@gmail.com>
2021-03-13 21:30:21 +0500

    Init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import * as core from '@actions/core';
+import { getInput, setOutput } from '@actions/core';
+
+async function run() {
+  try {
+    const nameToGreet = getInput('who-to-greet');
+    console.log(`Hello, ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    setOutput("time", time);
+  } 
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
