90ca2c40527c13a6b00d11b46bf7c374269405dd
danil-kuk <danilkuk5@gmail.com>
2021-03-15 20:48:19 +0500

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,33 @@
+import fetch from 'node-fetch';
 import * as core from '@actions/core';
 import { getInput, setOutput } from '@actions/core';
-import fetch from 'node-fetch';
 
 async function run() {
   try {
-    const nameToGreet = getInput('who-to-greet');
-    console.log(`Hello, ${nameToGreet}!`);
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const json = await response.json();
-    console.log('Http reques example', json)
-    const time = (new Date()).toTimeString();
-    setOutput("time", time);
+    greeting()
+    await httpRequest();
+    getTime()
   }
   catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function greeting() {
+  const nameToGreet = getInput('who-to-greet');
+  console.log(`Hello, ${nameToGreet}!`);
+  console.log(process.env.MY_VAR);
+}
+
+async function httpRequest() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const json = await response.json();
+  console.log('Http reques example', json)
+}
+
+function getTime() {
+  const time = (new Date()).toTimeString();
+  setOutput("time", time);
 }
 
 run()
